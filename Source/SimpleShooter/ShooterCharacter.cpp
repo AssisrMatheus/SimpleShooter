7249a97c2060fa5df2a8298881c2c0f16487e75d
AssisrMatheus <assisr.matheus@gmail.com>
2021-03-18 07:59:27 -0300

    playing death animation

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -61,6 +61,11 @@ float AShooterCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Dama
 	return Health;
 }
 
+bool AShooterCharacter::IsDead() const
+{
+	return Health <= 0;
+}
+
 void AShooterCharacter::MoveForward(float AxisValue)
 {
 	AddMovementInput(GetActorForwardVector() *	AxisValue);
