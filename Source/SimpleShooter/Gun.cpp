508bbe40578c163f1511b162e8fcb388abe638ab
AssisrMatheus <assisr.matheus@gmail.com>
2021-03-21 15:37:05 -0300

    spawning sounds

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -40,7 +40,7 @@ void AGun::PullTrigger()
 {
 	UE_LOG(LogTemp, Warning, TEXT("You pulled the trigger!"));
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
-
+	UGameplayStatics::SpawnSoundAttached(MuzzleSound, Mesh, TEXT("MuzzleFlashSocket"));
 	
 	FHitResult Hit;
 	FVector ShotDirection;
@@ -49,6 +49,7 @@ void AGun::PullTrigger()
 	if (GunTrace(Hit, ShotDirection))
 	{
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), ImpactSound, Hit.Location);
 
 		AActor* HitActor = Hit.GetActor();
 		if (HitActor)
